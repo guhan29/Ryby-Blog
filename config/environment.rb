3104f830817f2cc6fe734d389076654edcb3e8e4
guhan29 <mguhan439@gmail.com>
2020-07-29 08:29:29 +0530

    added configuration in evironment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               ENV["MAIL_HOST"],
@@ -15,3 +12,4 @@ config.action_mailer.smtp_settings = {
     authentication:       'plain',
     # enable_starttls_auto: true
 }
+ActionMailer::Base.delivery_method = :smtp
